2dd1fbb880e757b3198430319ed2dcacc33b2bfb
unknown <emonidi@gmail.com>
2015-11-09 08:20:16 +0200

    fixed image-field directive

--- app/scripts/directives/image-field.js
+++ app/scripts/directives/image-field.js
@@ -9,18 +9,20 @@
 angular.module('carshopApp')
   .directive('imageField', function ($parse) {
     return {
-      templateUrl:'../views/templates/image-field.html',
+      template:'<div class="form-group"><label for="image">Снимка</label><img class="image" style="display:none;width:100%;margin-bottom:10px;"/><input type="file" id="image" name="image" class="form-control"/></div>',
       restrict: 'A',
       scope:{
         image:'=model'
       },
       link: function postLink(scope, element, attrs) {
         var model = $parse(attrs.ngModel)(scope);
+        var image = element.find('.image');
         element.on('change',function(){
           var input = document.getElementById('image').files[0];
           var fileReader = new FileReader();
           fileReader.onload  = function(e){
               scope.image = fileReader.result
+              image.attr('src',scope.image).show();
           };
 
           fileReader.readAsDataURL(input)
